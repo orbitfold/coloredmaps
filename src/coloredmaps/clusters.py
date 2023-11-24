23a0cd4ec206f2a0843c3d5176926468c9c480c3
Jancauskas <janc_vy@eoc-001470l.intra.dlr.de>
2023-11-24 10:36:44 +0100

    use cv2 for reading images

--- src/coloredmaps/clusters.py
+++ src/coloredmaps/clusters.py
@@ -1,6 +1,4 @@
 from sklearn.cluster import MeanShift, estimate_bandwidth
-import rasterio
-from rasterio.enums import Resampling
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
@@ -19,39 +17,34 @@ def find_clusters(image_path, output_dir, sharpen_times=0, n_samples=100000, ban
     """Finds clusters in a given map in the RGB space.
     """
     filename = pathlib.Path(image_path).stem
-    with rasterio.open(image_path) as src:
-        profile = src.profile
-        orig_data = src.read()
-        data = orig_data.transpose()
-        for _ in range(sharpen_times):
-            data = sharpen(data)
-        shape = data.shape
-        data = data.reshape(shape[0] * shape[1], 3)
-        if bandwidth == -1:
-            bandwidth = estimate_bandwidth(data[np.random.choice(data.shape[0], 10000)])
-            print(f"Estimated bandwidth: {bandwidth}")
-        clustering = MeanShift(bandwidth=bandwidth, n_jobs=n_jobs).fit(data[np.random.choice(data.shape[0], n_samples)])
-        results = {}
-        predicted_labels = clustering.predict(data)
-        labels = np.unique(predicted_labels)
-        nodata = 255
-        profile['nodata'] = nodata
-        for label in labels:
-            if label != -1:
-                center = clustering.cluster_centers_[label]
-                size = data[predicted_labels == label].shape[0]
-                class_data = np.ones((shape[0] * shape[1], 3)) * nodata
-                class_data[predicted_labels == label] = data[predicted_labels == label]
-                class_data = class_data.reshape((shape[0], shape[1], 3))
-                class_data = class_data.transpose()
-                class_output = pathlib.Path(output_dir) / f"{filename}_{label}.tiff"
-                with rasterio.open(class_output, 'w', **profile) as dst:
-                    dst.write(class_data)
-                results[str(label)] = {'center' : [int(x) for x in list(clustering.cluster_centers_[label])], 'size' : int(size)}
-        with open(pathlib.Path(output_dir) / f"{filename}.json", 'w') as fd:
-            json.dump(results, fd)
-        plt.pie([results[p]['size'] for p in results], colors=[[x / 255.0 for x in results[p]['center']] for p in results])
-        plt.savefig(pathlib.Path(output_dir) / f"{filename}_colors.svg")
+    data = cv2.imread(image_path)
+    data = cv2.cvtColor(data, cv2.COLOR_BGR2RGB)    
+    for _ in range(sharpen_times):
+        data = sharpen(data)
+    shape = data.shape
+    data = data.reshape(shape[0] * shape[1], 3)
+    if bandwidth == -1:
+        bandwidth = estimate_bandwidth(data[np.random.choice(data.shape[0], 10000)])
+    print(f"Bandwidth value in use: {bandwidth}")
+    clustering = MeanShift(bandwidth=bandwidth, n_jobs=n_jobs).fit(data[np.random.choice(data.shape[0], n_samples)])
+    results = {}
+    predicted_labels = clustering.predict(data)
+    labels = np.unique(predicted_labels)
+    nodata = 255
+    for label in labels:
+        if label != -1:
+            center = clustering.cluster_centers_[label]
+            size = data[predicted_labels == label].shape[0]
+            class_data = np.ones((shape[0] * shape[1], 3)) * nodata
+            class_data[predicted_labels == label] = data[predicted_labels == label]
+            class_data = class_data.reshape((shape[0], shape[1], 3))
+            class_output = pathlib.Path(output_dir) / f"{filename}_{label}.tiff"
+            cv2.imwrite(str(class_output), cv2.cvtColor(class_data.astype(np.uint8), cv2.COLOR_RGB2BGR))
+            results[str(label)] = {'center' : [int(x) for x in list(clustering.cluster_centers_[label])], 'size' : int(size)}
+    with open(pathlib.Path(output_dir) / f"{filename}.json", 'w') as fd:
+        json.dump(results, fd)
+    plt.pie([results[p]['size'] for p in results], colors=[[x / 255.0 for x in results[p]['center']] for p in results])
+    plt.savefig(pathlib.Path(output_dir) / f"{filename}_colors.svg")
 
 @click.command()
 @click.option('-i', '--input-file', help='Input TIFF file')
