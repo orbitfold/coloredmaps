b76305ca8a8cd7d26e88508a23e5faca2bab8f4e
Jancauskas <janc_vy@eoc-001470l.intra.dlr.de>
2023-12-04 14:04:09 +0100

    added knn.py

---
+++ src/coloredmaps/knn.py
@@ -0,0 +1,77 @@
+import click
+import pathlib
+import json
+import os
+import pandas as pd
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+
+COLORED = [
+  '28_6404_4_fbg.jpg', '28_6506_4_fbg.jpg', '28_6605_D.jpg', 
+  '28_6607_D.jpg', '28_6608_D.jpg', '28_6609_D.jpg', 
+  '29_6508_4_fbg.jpg', '29_6610_D.jpg', '29_6705_SuGmbl.jpg', 
+  '29_6707_D.jpg', 'BayHStA_OBB_12856_0001.tif', 'BayHStA_OBB_KuPl_6811_0013.tif',
+  'M-32-10-A-a-Vienenburg-1984.bmp', 'M-32-10-A-b-Osterwieck-1985.bmp', 'M-32-10-A-c-Bad_Harzburg-1984.bmp',
+  'M-32-10-A-d-Ilsenburg_(Harz)-1985.bmp', 'M-32-10-A-c-Bad_Harzburg-1984.bmp', 'M-32-10-A-d-Ilsenburg_(Harz)-1985.bmp',
+  'M-32-10-B-a-Dardesheim-1985.bmp', 'M-32-10-B-b-Badersleben-1985.bmp', 'M-32-10-B-c-Wernigerode-1985.bmp',
+  'M-32-10-B-d-Derenburg-1985.bmp', 'M-32-10-C-a-Brocken-1984.bmp', 'M-32-10-C-b-Schierke-1985.bmp',
+  'MA_Karten_und_Plaene_308_0001.tif', 'MA_Karten_und_Plaene_308_0003.tif', 'N-32-72-D-a-Ostseebad_Kuehlungsborn-1989.bmp', 
+  'N-32-72-D-b-Bad_Doberan-1989.bmp', 'N-32-82-A-b-Bad_Schwartau-1984.bmp', 'N-32-82-D-c-Ratzeburg-1990.bmp', 
+  'N-32-83-B-d-Wismar-1990.bmp', 'N-32-94-D-b-Bennin-1989.bmp', 'N-32-95-A-d-Duemmer-1990.bmp',
+  'N-32-95-B-b-Schwerin-1990.bmp', 'PLS_9771_01.tif'
+]
+
+MONOCHROME = [
+  'df_dk_0010001_1725.tif', 'df_dk_0010001_2343.tif', 'df_dk_0010001_4643_1938.tif',
+  'df_dk_0010001_4902_1934.tif', 'df_dk_0010001_6714.tif', 'Landesvermessungsamt-Flurkartenarchiv_0002.tif',
+  'Landesvermessungsamt-Flurkartenarchiv_0003.tif', 'MA_Karten_und_Plaene_315_0001.tif', 'MA_Karten_und_Plaene_315_0002.tif',
+  'MA_Karten_und_Plaene_315_0003.tif', 'MA_Karten_und_Plaene_315_0004.tif', 'MA_Karten_und_Plaene_318_0023.tif',
+  'MA_Karten_und_Plaene_318_0024.tif', 'MA_Karten_und_Plaene_318_0025.tif', 'MA_Karten_und_Plaene_318_0026.tif',
+  'MA_Karten_und_Plaene_318_0027.tif', 'MA_Karten_und_Plaene_318_0028.tif', 'MA_Karten_und_Plaene_318_0029.tif',
+  'MA_Karten_und_Plaene_393_0001.tif', 'MA_Karten_und_Plaene_393_0002.tif', 'MA_Karten_und_Plaene_393_0003.tif',
+  'MA_Karten_und_Plaene_394_0001.tif', 'MA_Karten_und_Plaene_394_0002.tif', 'MF_KuPL_332_2_0001.tif',
+  'MF_KuPL_380_0001.tif', 'Pls_8540_0008.tif', 'Pls_8540_0014.tif' 
+]
+
+@click.command()
+@click.option('-i', '--input-dir', help='Input directory')
+@click.option('-d', '--dataset', help='Dataset')
+@click.option('-n', '--neighbors', help='How many neighbors')
+def main(input_dir, dataset, neighbors):
+  dataset = pd.read_csv(dataset)
+  neigh = KNeighborsClassifier(n_neighbors=3)
+  neigh.fit(np.array([dataset.r, dataset.g, dataset.b]).transpose(), dataset['class'])
+  confusion = {
+    'colored': {'colored': 0, 'monochrome': 0},
+    'monochrome': {'colored': 0, 'monochrome': 0}
+  }
+  for c_map in COLORED:
+    stem = pathlib.Path(c_map).stem
+    with open(os.path.join(input_dir, f"{stem}", f"{stem}.json"), 'r') as fd:
+      data = json.load(fd)
+      colored = 0
+      for key in data:
+        prediction = neigh.predict([data[key]['center']])
+        if prediction[0] == 2:
+          colored += 1
+      if colored > 0:
+        confusion['colored']['colored'] += 1
+      else:
+        confusion['colored']['monochrome'] += 1
+  for m_map in MONOCHROME:
+    stem = pathlib.Path(m_map).stem
+    with open(os.path.join(input_dir, f"{stem}", f"{stem}.json"), 'r') as fd:
+      data = json.load(fd)
+      colored = 0
+      for key in data:
+        prediction = neigh.predict([data[key]['center']])
+        if prediction[0] == 2:
+          colored += 1
+      if colored > 0:
+        confusion['monochrome']['colored'] += 1
+      else:
+        confusion['monochrome']['monochrome'] += 1
+  print(confusion)
+
+if __name__ == '__main__':
+  main()
